91a671cdd15ad22d236245c1022aa088e1f280c1
Ryan Fan <ryanfan1996@gmail.com>
2019-12-27 00:29:37 -0500

    anime js installed, menu animation partially complete

--- portfolio-web/src/menu/menu.js
+++ portfolio-web/src/menu/menu.js
@@ -1,16 +1,69 @@
 import React, { Component } from 'react';
 import './menu.css';
 
+import anime from 'animejs';
+
 export default class Menu extends Component {
+  menuRef = React.createRef();
+
   constructor(props) {
     super(props);
 
     this.state = {
       path: "",
       isMenuExpanded: false,
+      windowWidth: 0,
+      windowHeight: 0,
     };
 
     this.toggleMenu = this.toggleMenu.bind(this);
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ windowWidth: window.innerWidth, windowHeight: window.innerHeight });
+  }
+
+  componentDidUpdate() {
+    let animeTimeline = anime.timeline();
+    let menuExpandedWidth = (this.state.windowWidth * 0.8).toString();
+
+    let menuAnimation = {
+      targets: this.menuRef.current,
+      width: {
+        value: menuExpandedWidth,
+        duration: 500,
+        easing: "easeOutCubic"
+      },
+      easing: "easeOutCubic",
+      autoplay: false
+    };
+    let menuAnimationReverse = {
+      targets: this.menuRef.current,
+      width: {
+        value: '70px',
+        duration: 500,
+        easing: "easeOutCubic"
+      },
+      easing: "easeOutCubic",
+      autoplay: false
+    };
+
+    if (this.state.isMenuExpanded) {
+      animeTimeline.add(menuAnimation);
+    }
+    else {
+      animeTimeline.add(menuAnimationReverse);
+    }
   }
 
   // when user clicked the menu button
@@ -20,7 +73,10 @@ export default class Menu extends Component {
 
   render() {
     return (
-      <div className={this.state.isMenuExpanded ? "menu-container menu-open" : "menu-container"}>
+      <div
+        className={"menu-container"}
+        ref={this.menuRef}
+      >
         <div className="menu-button-wrapper">
           <div className="menu-bt" onClick={this.toggleMenu}>
             <div className="menu-bt-rt-1"></div>
