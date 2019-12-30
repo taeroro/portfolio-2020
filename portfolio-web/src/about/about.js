e9b3468a98fd1062605984b369a6c1b657dae7dc
Ryan Fan <ryanfan1996@gmail.com>
2019-12-29 22:21:48 -0500

    about done

--- portfolio-web/src/about/about.js
+++ portfolio-web/src/about/about.js
@@ -18,6 +18,11 @@ export default class About extends Component {
 
     this.introNameRef = null;
     this.bioRef = null;
+    this.contactRef = null;
+  }
+
+  componentDidUpdate(prevProps) {
+    window.scrollTo(0, 0);
   }
 
   componentDidMount() {
@@ -25,6 +30,7 @@ export default class About extends Component {
       setTimeout(() => {
         this.setState({ nameMouseIsOver: false });
         this.tl.to(this.bioRef, {duration: 0.5, opacity: 1, y: 0, ease: "cubic-bezier(0.215, 0.61, 0.355, 1)"}, "+=0.25");
+        this.tl.to(this.contactRef, {duration: 0.5, opacity: 1, y: 0, ease: "cubic-bezier(0.215, 0.61, 0.355, 1)"}, "-=0.25");
       }, 250);
     }
     else {
@@ -50,10 +56,20 @@ export default class About extends Component {
         </div>
 
         <p className="bio" ref={p => this.bioRef = p}>
-          is a digital product designer and engineer with a concentration in the practice of user experience design. He holds a Bachelor of Science degree in Computer Science from Purdue University(2019), and attaining a Masters of Professional Studies degree in Communication Design at Parsons School of Design(2020).
+          is a digital product designer and engineer with a concentration in the practice of user experience design. He holds a Bachelor of Science degree in Computer Science from Purdue University (2019), and attaining a Masters of Professional Studies degree in Communication Design at Parsons School of Design (2020).
           <br /><br />
           With the rapidly growing demand in digital product design, he stands at the intersection of design and engineering, positioning himself as the liaison of cooperating these interdisciplinary fields together to facilitate and create thoughtful user experience of the digital products.
         </p>
+
+        <div className="contact-wrapper" ref={div => this.contactRef = div}>
+          <h2 className="contact">CONTACT</h2>
+          <div className="another-contact-wrapper">
+            <p>Email: <a href="mailto:ryanfan1996@gmail.com">ryanfan1996@gmail.com</a></p>
+            <a href="https://www.linkedin.com/in/zuyuanfan/" target="_blank">LinkedIn</a>
+            <a href="https://www.instagram.com/ryantf11/" target="_blank">Instagram</a>
+            <a href="https://github.com/taeroro" target="_blank">Github</a>
+          </div>
+        </div>
       </div>
     );
   }
